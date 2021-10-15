d072fcce9cdb450b03af57bfabb519c3071504ce
Zdenek Preisler <z.preisler@gmail.com>
2021-10-15 15:27:26 +0000

    train

--- train.py
+++ train.py
@@ -2,9 +2,8 @@
 import torch
 from torch import nn,optim
 from torch.utils.data import Dataset,DataLoader
-from torch.optim import Adam
 
-from matplotlib.pyplot import show,figure,imshow,draw,ion,pause,subplots,subplots_adjust
+#from matplotlib.pyplot import show,figure,imshow,draw,ion,pause,subplots,subplots_adjust
 from numpy import log,array,asarray,save
 
 from src.nets import CNNSkip
@@ -13,9 +12,11 @@ from src.dataset import SpectraDataset
 
 from argparse import ArgumentParser
 from pathlib import Path
+from glob import glob
 
 import h5py,yaml
 import gc
+import os
 
 def main():
     """
@@ -25,6 +26,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('config_file')
     parser.add_argument('-n','--name',default=None)
+    parser.add_argument('-l','--learning_rate',default=None,type=float)
     args = parser.parse_args()
 
     with open(args.config_file,'r') as file:
@@ -33,15 +35,22 @@ def main():
     print('config:',config)
     print('args:',args)
 
-    if args.name:
-        name = args.name
+    for k,v in args.__dict__.items():
+        if v: config[k] = v
+
+    print('updated_config:',config)
+
+    #Path(config['name']).mkdir(parents=True, exist_ok=True)
+    try:
+        os.mkdir(config['name'])
+    except OSError as error:
+        print(error)
+
+    with open(config['name'] + '/config.yaml', 'w') as file:
+        yaml.dump(config,file)
     
     train_data = SpectraDataset(config['data'])
     train_dataloader = DataLoader(train_data,batch_size = config['batch_size'],shuffle=True)
-
-    inputs,targets = next(iter(train_dataloader))
-    print(inputs.shape)
-    print(targets.shape)
     
     torch.set_num_threads(config['num_threads'])
 
@@ -58,11 +67,19 @@ def main():
     model.train()
     model.to(device)
 
-    #checkpoint = torch.load()
-    #model.load_state_dict(checkpoint['model_state_dict'],strict=False)
-    #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    current_epoch = 1
+    checkpoints = sorted(glob(config['name'] + '/*.pth'))
+    if checkpoints:
+        print('Loading Last Checkpoint:',checkpoints[-1])
+        checkpoint = torch.load(checkpoints[-1])
+
+        model.load_state_dict(checkpoint['model_state_dict'],strict=False)
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        current_epoch = checkpoint['epoch'] + 1
     
-    for epoch in range(config['n_epochs']):
+    for epoch in range(current_epoch,config['n_epochs']):
+        print('epoch:',epoch)
+
         for j,_batch in enumerate(train_dataloader):
 
             inputs,targets = _batch
@@ -73,11 +90,23 @@ def main():
             outputs = model(_inputs)
 
             loss = criterion(outputs,_targets)
-            print(loss)
 
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+
+        if epoch % 1 == 0:
+            path = config['name'] + '/%04d.pth'%epoch
+
+            print('saving:',path)
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'config': config,
+                'loss': loss},
+                path)
+
 
 
 if __name__ == '__main__':
